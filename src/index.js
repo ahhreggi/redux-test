02656a1dec9c742ca40653a84d219ed0b62c022a
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-05 16:30:06 -0400

    Install redux, add imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION -> E.G., INCREMENT
+
+// REDUCER -> MODIFIES STATE DEPENDING ON THE GIVEN ACTION
+
+// DISPATCH -> "Dispatch this action to the reducer"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
